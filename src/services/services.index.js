11fddbac9ef4ba39efb26d659b1d1516790e0811
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-03 00:12:59 -0300

    Added services index

---
+++ src/services/services.index.js
@@ -0,0 +1,5 @@
+export { CreateProductUseCase } from "./createProduct";
+export { DeleteProductUseCase } from "./deleteProduct";
+export { getAllProductsUseCase } from "./getAllProducts";
+export { GetProductByIdUseCase } from "./getProductById";
+export { UpdateProductUseCase } from "./updateProduct";
