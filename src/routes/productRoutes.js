0cd95b1ca3fcfe25a12ccbb76f2d57d6d44f5330
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-03 11:16:35 -0300

    Added this.controllers

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,19 +5,20 @@ export class ProductRoutes {
   }
 
   route() {
-    this.router.get("/", controller.getAllProductsController(req, res));
-    this.router.get(
-      "/product/:id",
-      controller.getProductByIdController(req, res)
+    this.router.get("/", (req, res) =>
+      this.controller.getAllProductsController(req, res)
     );
-    this.router.delete(
-      "/delete/:id",
-      controller.deleteProductController(req, res)
+    this.router.get("/product/:id", (req, res) =>
+      this.controller.getProductByIdController(req, res)
     );
-    this.router.post("/create", controller.createProductController(req, res));
-    this.router.put(
-      "/update/:id",
-      controller.updateProductController(req, res)
+    this.router.delete("/delete/:id", (req, res) =>
+      this.controller.deleteProductController(req, res)
+    );
+    this.router.post("/create", (req, res) =>
+      this.controller.createProductController(req, res)
+    );
+    this.router.put("/update/:id", (req, res) =>
+      this.controller.updateProductController(req, res)
     );
 
     return this.router;
