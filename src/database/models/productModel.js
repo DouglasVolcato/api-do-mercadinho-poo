d5a600d8feb9f6202f2e546dcbf7afee9b37843a
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-03 12:06:53 -0300

    Added id

--- src/database/models/productModel.js
+++ src/database/models/productModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const productsSchema = new mongoose.Schema({
+  id: { type: String, required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
