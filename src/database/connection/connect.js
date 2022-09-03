53f642f4fe425a258f79ac31a333391d920f0dee
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-03 01:55:34 -0300

    Added connection to MongoDB

---
+++ src/database/connection/connect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export class MongoDbConnection {
+  async connectDb() {
+    await mongoose
+      .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("Connected to MongoDb"))
+      .catch((err) => console.log("Error: " + err));
+  }
+}
